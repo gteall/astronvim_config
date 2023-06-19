5fe33b99335cb5d3a5c8c472748f8ec131bed961
gtea <liliang_subu@163.com>
2023-06-19 09:41:20 +0800

    增加快速切换buffer的快捷键

--- mappings.lua
+++ mappings.lua
@@ -20,6 +20,13 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    ["<S-h>"] = {function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer"},
+    ["<S-l>"] = {
+      function() 
+        require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) 
+      end, 
+      desc = "Next buffer"},
   },
   t = {
     -- setting a mapping to false will disable it
